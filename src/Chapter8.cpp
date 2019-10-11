9acd1cedd6af5c064a3e32908533e4e794f775f2
xiaoyi <xiaoyi@xiaoyi-ubuntu16.04>
2019-10-12 00:30:32 +0800

    linxiaoyi_1012

--- src/Chapter8.cpp
+++ src/Chapter8.cpp
@@ -42,6 +42,8 @@ int main()
 
 
 
+
+/*
 #include <iostream>
 int main()
 {
@@ -56,4 +58,85 @@ int main()
     std::cout << "bussies address = " << &bussies << " , rats address is " << &rats << ", rodents address is " << &rodents<<std::endl;
     return 0;
 }
+*/
 
+
+
+
+
+
+
+
+/*
+#include <iostream>
+void swapByValue(int x, int y);
+void swapByReferences(int& x, int& y);
+void swapByPoint(int* x, int* y);
+int main()
+{
+    int wallet1 = 300;
+    int wallet2 = 350;
+
+    std::cout << "wallet1 = $" << wallet1 << " wallet2 = $"<<wallet2<<std::endl;
+    std::cout << "Using references to swap contents" << std::endl;
+    swapByReferences(wallet1, wallet2);
+    std::cout << "wallet1 = $" << wallet1 << " wallet2 = $"<<wallet2<<std::endl;
+    std::cout << "Using Pointers to swap contents again:" <<std::endl;
+    swapByPoint(&wallet1,&wallet2);
+    std::cout << "wallet1 = $" << wallet1 << " wallet2 = $"<<wallet2<<std::endl;  
+    std::cout << "Try to use passing by value:"<<std::endl;
+    swapByValue(wallet1,wallet2);
+    std::cout << "wallet1 = $" << wallet1 << " wallet2 = $"<<wallet2<<std::endl;  
+    return 0;
+}
+
+void swapByValue(int x, int y)
+{
+    int temp;
+    temp = y;
+    y = x;
+    x = temp;
+}
+
+void swapByReferences(int& x, int& y)
+{
+    int temp;
+    temp = y;
+    y = x;
+    x = temp;
+}
+
+void swapByPoint(int* x, int* y)
+{
+    int temp;
+    temp = *y;
+    *y = *x;
+    *x = temp;
+}
+*/
+
+
+
+
+#include <iostream>
+double cube(double x);
+double rfCube(double& a);
+int main()
+{
+    double x =3;
+    std::cout << cube(x) << " = cube of "<< x <<std::endl;
+    std::cout << rfCube(x) << " = cube of " << x<<std::endl;
+    return 0;
+}
+
+double cube(double x)
+{
+    x *= x *x;
+    return x;
+}
+
+double rfCube(double& a)
+{
+    a *= a * a;
+    return a;
+}
