697c753d0f4f5d0adc7225252595d6a5464b43b0
xiaoyi <linyihuang@hitech.com>
2019-10-17 17:59:07 +0800

    linxiaoyi_1017

--- src/Chapter8.cpp
+++ src/Chapter8.cpp
@@ -181,3 +181,66 @@ const snap& use(snap& sysopref)
 }
 */
 
+
+
+
+
+
+
+/*
+#include <iostream>
+#include <string>
+std::string version0(const std::string& s1, const std:: string&  s2);
+std::string& version1(std::string& s1, const std::string& s2);
+std::string& version2(const std::string& s1, const std::string& s2);
+int main()
+{
+    std::string input;
+    std::string copy;
+    std::string result;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, input);
+    copy = input;
+    std::cout << "Your string as entered: " << input << std::endl;
+    result = version0(input, "***");
+    std::cout << "Your string enhanced:  "<< result <<std::endl;
+    std::cout << "Your original string: " << input << std::endl;
+
+    result = version1(input, "###" );
+    std::cout << "Your string enhanced: " << result <<std::endl;
+    std::cout << "Your original string: " << input << std::endl;
+
+    std::cout << "Resetting original string." <<std::endl;
+    input = copy;
+    result = version2(input, "@@@");
+    std::cout << "Your string enhanced: " << result <<std::endl;
+    std::cout << "Your original string: " << input << std::endl;
+    return 0;
+}
+
+std::string version0(const std::string& s1, const std::string& s2)
+{
+    std::string temp;
+    temp =s2 + s1 +s2;
+    return  temp;
+}
+
+std::string& version1(std::string& s1, const std::string& s2)
+{
+    s1 = s2 + s1 +s2;
+    return  s1;
+}
+
+std::string& version2(const std::string& s1, const std::string& s2)
+{
+    std::string temp;
+    temp = s2 + s1 +s2;
+    return temp;
+}
+*/
+
+
+
+
+
+
