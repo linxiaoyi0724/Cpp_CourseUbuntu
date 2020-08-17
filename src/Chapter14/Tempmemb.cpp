2e3e7c8bcb4469a98ce9745f05827f545ccf1eae
xiaoyi <781210678@qq.com>
2020-08-18 00:25:53 +0800

    linxiaoyi_20200818

--- src/Chapter14/Tempmemb.cpp
+++ src/Chapter14/Tempmemb.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-using namespace std;
-template <typename T>
-class beta
-{
-private:
-	template<typename V>
-	class hold
-	{
-	private:
-		V val;
-	public:
-		hold(V v = 0):val(v){}
-		void show()const { cout << val << endl; }
-		V Value()const { return val; }
-	};
+// #include <iostream>
+// using namespace std;
+// template <typename T>
+// class beta
+// {
+// private:
+// 	template<typename V>
+// 	class hold
+// 	{
+// 	private:
+// 		V val;
+// 	public:
+// 		hold(V v = 0):val(v){}
+// 		void show()const { cout << val << endl; }
+// 		V Value()const { return val; }
+// 	};
 
-	hold<T> q;
-	hold<int> n;
+// 	hold<T> q;
+// 	hold<int> n;
 
-public:
-	beta(T t,int i):q(t),n(i){}
-	template<typename U>
-	U blab(U u, T t) { return (n.Value() + q.Value())* u / t; }
-	void show()const { q.show(); n.show(); }
-};
+// public:
+// 	beta(T t,int i):q(t),n(i){}
+// 	template<typename U>
+// 	U blab(U u, T t) { return (n.Value() + q.Value())* u / t; }
+// 	void show()const { q.show(); n.show(); }
+// };
 
-int main()
-{
-	beta<double> guy(3.5, 3);
-	guy.show();
-	cout << guy.blab(10.0, 2.3) << endl;
-	cout << "Done!" << endl;
-	return 0;
-}
+// int main()
+// {
+// 	beta<double> guy(3.5, 3);
+// 	guy.show();
+// 	cout << guy.blab(10.0, 2.3) << endl;
+// 	cout << "Done!" << endl;
+// 	return 0;
+// }
---
+++ src/Chapter14/tempparm.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "stacktp.h"
+
+template<template <typename T> class Thing>
+class Crab
+{
+private:
+    Thing<int> s1;
+    Thing<double> s2;
+
+public:
+    Crab(){}
+    bool push(int a, double x){return s1.Push(a) && s2.Push(x);}
+    bool pop(int& a, double& x){return s1.Pop(a) && s2.Pop(x);}
+};
+
+int main()
+{
+    using namespace std;
+    Crab<Stack> nebela;
+    int n1;
+    double nb;
+    cout << "Enter double pairs, such as 4 3.5(0 0 to be end): "<<endl;
+    while (cin >> n1 >> nb && n1 > 0 && nb >0)
+    {
+        if(!nebela.push(n1,nb))
+            break;
+    }
+    while (nebela.pop(n1, nb))
+    {
+        cout << n1 << ", " << nb <<endl;
+    }
+    cout << "Done." <<endl;
+    return 0;
+}
