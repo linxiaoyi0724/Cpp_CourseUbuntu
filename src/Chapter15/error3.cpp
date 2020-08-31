98346764bc1c91d0562bf8b6a104a752401e3c77
xiaoyi <linyihuang@hitech.com>
2020-08-31 14:07:54 +0800

    linxiaoyi_20200831

--- src/Chapter15/error3.cpp
+++ src/Chapter15/error3.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-using namespace std;
-
-double Hmean(double x, double y);
-int main()
-{
-	double x, y, z;
-	cout << "please input two number: ";
-	while (cin >>x>>y)
-	{
-		try
-		{
-			z = Hmean(x, y);
-		}
-		catch(const char* s)
-		{
-			std::cout << s << endl;
-			cout << "Enter two numbers: ";
-			continue;
-		}
-		cout << x << " and " << y << " hmean is  " << z << endl;
-		cout << "Enter two numbers: ";
-	}
-}
-
-double Hmean(double x, double y)
-{
-	if (x == -y)
-	{
-		throw "bad hmean() ";
-	}
-	return 2.0* x*y / (x + y);
-}
+//#include <iostream>
+//using namespace std;
+//
+//double Hmean(double x, double y);
+//int main()
+//{
+//	double x, y, z;
+//	cout << "please input two number: ";
+//	while (cin >>x>>y)
+//	{
+//		try
+//		{
+//			z = Hmean(x, y);
+//		}
+//		catch(const char* s)
+//		{
+//			std::cout << s << endl;
+//			cout << "Enter two numbers: ";
+//			continue;
+//		}
+//		cout << x << " and " << y << " hmean is  " << z << endl;
+//		cout << "Enter two numbers: ";
+//	}
+//}
+//
+//double Hmean(double x, double y)
+//{
+//	if (x == -y)
+//	{
+//		throw "bad hmean() ";
+//	}
+//	return 2.0* x*y / (x + y);
+//}
---
+++ src/Chapter16/smartptrs.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+class Report
+{
+private:
+	std::string str;
+public:
+	Report(const std::string s) :str(s) { std::cout << "Object created!" << std::endl; }
+	~Report() { std::cout << "Object deleted! " << std::endl; }
+	void comment()const { std::cout << str << std::endl; }
+};
+
+int main()
+{
+	//{
+	//	std::auto_ptr<Report> ps(new Report("using auto_ptr"));
+	//	ps->comment();
+	//}
+
+	//{
+	//	std::shared_ptr<Report> ps(new Report("using shared_ptr"));
+	//	ps->comment();
+	//}
+
+	//{
+	//	std::unique_ptr<Report> ps(new Report("using unique_ptr"));
+	//	ps->comment();
+	//}
+
+	std::unique_ptr<std::string> ps1(new std::string("Unique ptr"));
+	std::unique_ptr<std::string> ps2;
+	ps2 = std::move(ps1);
+	std::cout << *ps2 << std::endl;
+	return 0;
+}
